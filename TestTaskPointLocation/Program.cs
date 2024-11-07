d94b4255ec919479db5b1c19ed44287c51c53736
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-11-07 08:43:48 +0200

    rename methods

--- TestTaskPointLocation/Program.cs
+++ TestTaskPointLocation/Program.cs
@@ -17,48 +17,34 @@ namespace TestTaskPointLocation
             int x2;
             int y2;
 
-            SetSquareLength(out squareSide);
-            SetTopLeftCorner(out x1, out y1);
-            SetPoint(out x2, out y2);
+            GetSquareLength(out squareSide);
+            GetTopLeftCorner(out x1, out y1);
+            GetPoint(out x2, out y2);
             GetPointLocation(squareSide, x1, y1, x2, y2);
 
         }
-
-        private static void GetPointLocation(int squareSide, int x1, int y1, int x2, int y2)
+        private static void GetSquareLength(out int squareSide)
         {
-            if (x2 >= x1 && x2 <= x1 + squareSide && y2 >= y1 - squareSide && y2 <= y1)
-            {
-                Console.WriteLine("\n The point is inside the square.");
-            }
-            else
-            {
-                Console.WriteLine("\n The point is outside the square.");
-            }
-            Console.ReadKey();
-        }
-
-        private static void SetPoint(out int x2, out int y2)
-        {
-            Console.WriteLine("\n Enter the coordinates of the point (x2, y2):");
+            Console.WriteLine("\n Enter the side length of the square:");
 
             while (true)
             {
                 try
                 {
-                    Console.Write("x2:");
-                    x2 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("y2:");
-                    y2 = Convert.ToInt32(Console.ReadLine());
-                    break;
+                    squareSide = Convert.ToInt32(Console.ReadLine());
+                    if (squareSide > 0)
+                        break;
+                    else
+                        Console.WriteLine("Please enter a positive integer.");
                 }
                 catch
                 {
-                    Console.WriteLine("Please enter valid integers.");
+                    Console.WriteLine("Please enter a valid integer.");
                 }
             }
         }
 
-        private static void SetTopLeftCorner(out int x1, out int y1)
+        private static void GetTopLeftCorner(out int x1, out int y1)
         {
             Console.WriteLine("\n Enter the top-left corner coordinates of the square (x1, y1):");
 
@@ -79,26 +65,38 @@ namespace TestTaskPointLocation
                 }
             }
         }
-
-        private static void SetSquareLength(out int squareSide)
+        private static void GetPoint(out int x2, out int y2)
         {
-            Console.WriteLine("\n Enter the side length of the square:");
+            Console.WriteLine("\n Enter the coordinates of the point (x2, y2):");
 
             while (true)
             {
                 try
                 {
-                    squareSide = Convert.ToInt32(Console.ReadLine());
-                    if (squareSide > 0)
-                        break;
-                    else
-                        Console.WriteLine("Please enter a positive integer.");
+                    Console.Write("x2:");
+                    x2 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("y2:");
+                    y2 = Convert.ToInt32(Console.ReadLine());
+                    break;
                 }
                 catch
                 {
-                    Console.WriteLine("Please enter a valid integer.");
+                    Console.WriteLine("Please enter valid integers.");
                 }
             }
         }
+
+        private static void GetPointLocation(int squareSide, int x1, int y1, int x2, int y2)
+        {
+            if (x2 >= x1 && x2 <= x1 + squareSide && y2 >= y1 - squareSide && y2 <= y1)
+            {
+                Console.WriteLine("\n The point is inside the square.");
+            }
+            else
+            {
+                Console.WriteLine("\n The point is outside the square.");
+            }
+            Console.ReadKey();
+        }      
     }
 }
