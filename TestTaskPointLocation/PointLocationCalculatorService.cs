9f3254682f9a3db8be452c509ead7ab708843bd9
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-11-07 12:01:24 +0200

    add PointLocationCalculatorService

---
+++ TestTaskPointLocation/PointLocationCalculatorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+
+namespace TestTaskPointLocation
+{
+    internal class PointLocationCalculatorService
+    {
+        public bool CheckPointLocation(int squareSide, Point x1, Point y1, Point x2, Point y2)
+        {
+            if (x2.X >= x1.X && x2.X <= x1.X + squareSide && y2.Y >= y1.Y - squareSide && y2.Y <= y1.Y)
+                return true;
+            else
+                return false;
+        }
+    }
+}
